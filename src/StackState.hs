96eafb7bc96152ad1d72f75c7cbbf0e1de5a2255
Shu Gen <gen_shu@caddi.jp>
2022-09-19 20:58:53 +0900

    add StackState

---
+++ src/StackState.hs
@@ -0,0 +1,15 @@
+import Control.Monad.State
+
+push :: a -> State [a] ()
+push = modify . (:)
+
+pop :: State [a] a
+pop = do
+  value <- gets head
+  modify tail
+  return value
+
+applyTop :: (a -> a) -> State [a] ()
+applyTop f = do
+  a <- pop
+  push (f a)
